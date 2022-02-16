71d0db9396608ba177e01ca4d7ab8077f67ea989
Matt Haynes <Mattgh9152@gmail.com>
2022-02-16 10:26:46 +0000

    Init NextJS/Tailwind

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+        </div>
+    )
+}
