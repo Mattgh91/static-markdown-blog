d739d4e2e9a4a0859014c2005bb0d270770f5e53
Matt Haynes <Mattgh9152@gmail.com>
2022-02-16 14:47:41 +0000

    Set up homepage post props using fs & path; can use these n getStaticProps because Next JS removes them from your imports at runtime / only uses them server side

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import Layout from "@/components/Layout"
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
+    console.log(posts);
     return (
         <Layout>
             <h1 className="text-3xl font-bold underline">
@@ -8,4 +12,27 @@ export default function HomePage() {
             </h1>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join('posts'));
+
+    const posts = files.map(file => {
+        const slug = file.replace('.md', '');
+
+        const markdownWithMeta = fs.readFileSync(path.join('posts', file), 'utf-8');
+
+        const { data: frontMatter } = matter(markdownWithMeta);
+
+        return {
+            slug,
+            frontMatter,
+        }
+    });
+
+    return {
+        props: {
+            posts,
+        }
+    }
 }
