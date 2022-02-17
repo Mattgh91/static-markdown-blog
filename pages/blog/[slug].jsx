d6495e04b081e8e0bd4b562df075a129a2e7a19c
Matt Haynes <Mattgh9152@gmail.com>
2022-02-17 15:19:38 +0000

    Sort out the display of individual blog posts /w marked package/some styling, import gsap to play with some animation work

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,7 +1,11 @@
-import Layout from '@/components/Layout';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { marked } from 'marked';
+import Link from 'next/link';
+
+import Layout from '@/components/Layout';
+import CategoryLabel from '@/components/CategoryLabel';
 
 export default function BlogPostPage({
     frontmatter: {
@@ -15,10 +19,33 @@ export default function BlogPostPage({
     content,
     slug,
 }) {
+    console.log(marked);
     return (
-        <Layout>
-            <h1 className="text-5xl border-b-4 p-5 font-bold">{title}</h1>
-            {content}
+        <Layout title={title}>
+            <Link href='/blog'>Go Back</Link>
+            <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
+                <div className='flex justify-between items-center mt-4'>
+                    <h1 className='text-5xl mb-7'>{title}</h1>
+                    <CategoryLabel>{category}</CategoryLabel>
+                </div>
+                <img src={coverImage} alt='' className='w-full rounded' />
+
+                <div className='flex justify-between items-center bg-gray-100 p-2 my-8'>
+                    <div className='flex items-center'>
+                        <img
+                            src={authorImage}
+                            alt=''
+                            className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'
+                        />
+                        <h4>{author}</h4>
+                    </div>
+                    <div className='mr-4'>{date}</div>
+                </div>
+
+                <div className='blog-text mt-2'>
+                    <div dangerouslySetInnerHTML={{ __html: marked.parse(content) }}></div>
+                </div>
+            </div>
         </Layout>
     )
 }
